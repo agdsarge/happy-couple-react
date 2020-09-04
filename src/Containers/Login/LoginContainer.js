21d4e46051ae3df26099907b1725bd20896eb273
epittab <ep7m@me.com>
2020-09-04 15:14:17 -0400

    added boiler plate code to index.js and App.js for router and redux; imported login container component to App

---
+++ src/Containers/Login/LoginContainer.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+import './LoginContainer.css'
+
+class LoginContainer extends Component {
+render() {
+    return (
+        <div className='LoginContainer'>
+            <p>hi</p>
+        </div>
+        )
+    }
+}
+
+export default LoginContainer;
