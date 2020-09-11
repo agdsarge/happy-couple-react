69c0fff6b0b79327869e4f716db8779a2e4ea31c
agdsarge <agdsargeant@gmail.com>
2020-09-11 16:53:50 -0400

    some splash stuff

--- src/App.js
+++ src/App.js
@@ -9,23 +9,25 @@ import LoginContainer from './Containers/Login/LoginContainer';
 import RegisterContainer from './Containers/Register/RegisterContainer';
 import Home from './Containers/Home/Home';
 import ProtectedRoute from './Components/ProtectedRoute';
+import Splash from './Components/Splash'
 
 class App extends Component {
   
   render () {
-    return (
-      <div className="App">
-       
-      <Switch>
-        <ProtectedRoute isAuth={this.props.isAuth} path='/home' component={ Home } />
-        <Route exact path='/login' render={() => < LoginContainer /> } />
-        <Route exact path='/register' render={() => < RegisterContainer /> } />
+      return (
+        <div className="App">
+        
+        <Switch>
+          <ProtectedRoute isAuth={this.props.isAuth} path='/home' component={ Home } />
+          <Route exact path='/login' render={() => < LoginContainer /> } />
+          <Route exact path='/register' render={() => < RegisterContainer /> } />
+          <Route exact path='/' render={() => < Splash /> } />
 
-      </Switch>
-  
-  
-      </div>
-    );
+        </Switch>
+    
+    
+        </div>
+      );
   }
 }
 
---
+++ src/Components/Splash.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import './Splash.css'
+
+class Splash extends Component {
+    render() {
+        return (
+            <div className='splash-div'>
+                <p> Hello, world. Splash page here. </p>
+            </div>
+        )
+    }
+}
+
+export default Splash
