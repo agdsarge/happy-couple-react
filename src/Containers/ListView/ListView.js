d1ee4cf9bb0d95aa8943cc42ab91676985543eb7
agdsarge <agdsargeant@gmail.com>
2020-09-25 17:05:07 -0400

    accordion for home and listview

--- src/Containers/ListView/ListView.js
+++ src/Containers/ListView/ListView.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react'
-
+import Wizard from '../Wizard/Wizard'
+import ToDo from '../ToDo/ToDo'
+import Accordion from '@material-ui/core/Accordion';
+import AccordionSummary from '@material-ui/core/AccordionSummary';
+import AccordionDetails from '@material-ui/core/AccordionDetails';
 export default class ListView extends Component {
     render() {
         return (
-            <div>
-                <h1>LIST VIEW!!!</h1>
+            <div className='ListViewAccordion'>
+                <Accordion>
+                    <AccordionSummary>
+                        <h2>Wizard</h2>
+                    </AccordionSummary>
+                    <AccordionDetails>
+                        <Wizard />
+                    </AccordionDetails>
+                </Accordion>
+                <Accordion TransitionProps={{ unmountOnExit: true }}>
+                    <AccordionSummary>
+                        <h2>ToDo</h2>
+                    </AccordionSummary>
+                    <AccordionDetails>
+                        <ToDo />
+                    </AccordionDetails>
+                </Accordion>
             </div>
         )
     }
